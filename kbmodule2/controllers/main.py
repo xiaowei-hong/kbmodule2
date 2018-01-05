1863a9ddac105478f4cdfdd23ee7df1e0ed48932
ziyou_liu_local <26520751@qq.com>
2018-01-05 09:10:56 -0800

    xiaowei-hong

--- kbmodule2/controllers/main.py
+++ kbmodule2/controllers/main.py
@@ -12,14 +12,14 @@ from odoo.http import request
 class kbmodule2(http.Controller):
 
     @http.route(["/productsinfo/k801.htm"], type='http', auth="public", website=True)
-    def handler(self, s_action=None, db=None, **kw):
-		return http.local_redirect('http://www.premiumquartz.com/?p=1801')
+    	def handler1(*args, **kw):
+			return http.local_redirect('http://www.premiumquartz.com/?p=1801')
 		@http.route(["/productsinfo/k802.htm"], type='http', auth="public", website=True)
-    def handler(self, s_action=None, db=None, **kw):
-		return http.local_redirect('http://www.premiumquartz.com/?p=1802')
+    	def handler2(*args, **kw):
+			return http.local_redirect('http://www.premiumquartz.com/?p=1802')
 		@http.route(["/productsinfo"], type='http', auth="public", website=True)
-    def handler(self, s_action=None, db=None, **kw):
-		return http.local_redirect('http://www.premiumquartz.com/')
+    	def handler3(*args, **kw):
+			return http.local_redirect('http://www.premiumquartz.com/')
 		@http.route(["/productsinfo/k801"], type='http', auth="public", website=True)
-    def handler(self, s_action=None, db=None, **kw):
-		return http.local_redirect('http://www.premiumquartz.com/?p=1801')
+    	def handler5(*args, **kw):
+			return http.local_redirect('http://www.premiumquartz.com/?p=1801')
