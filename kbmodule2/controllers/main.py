c32ac06b8a91ee8841f818d84a143b6cacf757db
ziyou_liu_local <26520751@qq.com>
2018-01-05 11:02:58 -0800

    xiaowei-hong

--- kbmodule2/controllers/main.py
+++ kbmodule2/controllers/main.py
@@ -9,15 +9,150 @@ from odoo import http
 from odoo.http import request
 
 class kbmodule2(http.Controller):
- @http.route(["/productsinfo/k801.htm"], type='http', auth="public", website=True)
+ @http.route(["/productsinfo/k828.htm"], type='http', auth="public", website=True)
  def handler1(*args, **kw):
-  return http.local_redirect('http://www.premiumquartz.com/?p=1801')
- @http.route(["/productsinfo/k802.htm"], type='http', auth="public", website=True)
+  return http.local_redirect('http://www.premiumquartz.com/?p=1828')
+ @http.route(["/productsinfo/k826.htm"], type='http', auth="public", website=True)
  def handler2(*args, **kw):
-  return http.local_redirect('http://www.premiumquartz.com/?p=1802')
- @http.route(["/productsinfo"], type='http', auth="public", website=True)
+  return http.local_redirect('http://www.premiumquartz.com/?p=1826')
+ @http.route(["/productsinfo/k822.htm"], type='http', auth="public", website=True)
  def handler3(*args, **kw):
-  return http.local_redirect('http://www.premiumquartz.com/')
- @http.route(["/productsinfo/k801"], type='http', auth="public", website=True)
+  return http.local_redirect('http://www.premiumquartz.com/?p=1822')
+ @http.route(["/productsinfo/k821.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1821')
+ @http.route(["/productsinfo/k821.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1821')
+ @http.route(["/productsinfo/k820.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1820')
+ @http.route(["/productsinfo/k819.htm"], type='http', auth="public", website=True)
+ def handler1(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1819')
+ @http.route(["/productsinfo/k813.htm"], type='http', auth="public", website=True)
+ def handler2(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1813')
+ @http.route(["/productsinfo/k812.htm"], type='http', auth="public", website=True)
+ def handler3(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1812')
+ @http.route(["/productsinfo/k811.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1811')
+ @http.route(["/productsinfo/k808.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1808')
+ @http.route(["/productsinfo/k806.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1806')
+ @http.route(["/productsinfo/k804.htm"], type='http', auth="public", website=True)
+ def handler1(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1804')
+ @http.route(["/productsinfo/k803.htm"], type='http', auth="public", website=True)
+ def handler2(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1803')
+ @http.route(["/productsinfo/k802.htm"], type='http', auth="public", website=True)
+ def handler3(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1802')
+ @http.route(["/productsinfo/k801.htm"], type='http', auth="public", website=True)
  def handler5(*args, **kw):
   return http.local_redirect('http://www.premiumquartz.com/?p=1801')
+ @http.route(["/productsinfo/k508.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1508')
+ @http.route(["/productsinfo/k501.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1501')
+ @http.route(["/productsinfo/k436.htm"], type='http', auth="public", website=True)
+ def handler1(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1436')
+ @http.route(["/productsinfo/k435.htm"], type='http', auth="public", website=True)
+ def handler2(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1435')
+ @http.route(["/productsinfo/k432.htm"], type='http', auth="public", website=True)
+ def handler3(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1432')
+ @http.route(["/productsinfo/k430.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1430')
+ @http.route(["/productsinfo/k422.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1422')
+ @http.route(["/productsinfo/k421.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1421')
+ @http.route(["/productsinfo/k418.htm"], type='http', auth="public", website=True)
+ def handler1(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1418')
+ @http.route(["/productsinfo/k417.htm"], type='http', auth="public", website=True)
+ def handler2(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1417')
+ @http.route(["/productsinfo/k416.htm"], type='http', auth="public", website=True)
+ def handler3(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1416')
+ @http.route(["/productsinfo/k415.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1415')
+ @http.route(["/productsinfo/k414.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1414')
+ @http.route(["/productsinfo/k413.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1413')
+ @http.route(["/productsinfo/k412.htm"], type='http', auth="public", website=True)
+ def handler1(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1412')
+ @http.route(["/productsinfo/k411.htm"], type='http', auth="public", website=True)
+ def handler2(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1411')
+ @http.route(["/productsinfo/k403.htm"], type='http', auth="public", website=True)
+ def handler3(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1403')
+ @http.route(["/productsinfo/k307.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1307')
+ @http.route(["/productsinfo/k300.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1300')
+ @http.route(["/productsinfo/k216.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1216')
+ @http.route(["/productsinfo/k202.htm"], type='http', auth="public", website=True)
+ def handler1(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1202')
+ @http.route(["/productsinfo/k128.htm"], type='http', auth="public", website=True)
+ def handler2(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1128')
+ @http.route(["/productsinfo/k126.htm"], type='http', auth="public", website=True)
+ def handler3(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1126')
+ @http.route(["/productsinfo/k125.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1125')
+ @http.route(["/productsinfo/k123.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1123')
+ @http.route(["/productsinfo/k121.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1121')
+ @http.route(["/productsinfo/k111.htm"], type='http', auth="public", website=True)
+ def handler1(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1111')
+ @http.route(["/productsinfo/k110.htm"], type='http', auth="public", website=True)
+ def handler2(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1110')
+ @http.route(["/productsinfo/k109.htm"], type='http', auth="public", website=True)
+ def handler3(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1109')
+ @http.route(["/productsinfo/k108.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1108')
+ @http.route(["/productsinfo/k106.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1106')
+ @http.route(["/productsinfo/k103.htm"], type='http', auth="public", website=True)
+ def handler5(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1103')
+ @http.route(["/productsinfo/k102.htm"], type='http', auth="public", website=True)
+ def handler1(*args, **kw):
+  return http.local_redirect('http://www.premiumquartz.com/?p=1102')
