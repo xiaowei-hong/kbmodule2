ed07eaa943b1fb720d4447c0f00490fc4eaed7ee
ziyou_liu_local <26520751@qq.com>
2018-01-05 01:25:22 -0800

    xiaowei-hong

--- kbmodule1/controllers/main.py
+++ kbmodule1/controllers/main.py
@@ -12,8 +12,8 @@ from odoo.http import request
 class kbmodule1(http.Controller):
 
     @http.route(["/productsinfo/k801.htm"], type='http', auth="public", website=True)
-    def go_away(self):
+    def handler(self):
 		return http.local_redirect('http://www.premiumquartz.com/?p=1801')
 	@http.route(["/productsinfo/k802.htm"], type='http', auth="public", website=True)
-    def go_away(self):
+    def handler(self):
 		return http.local_redirect('http://www.premiumquartz.com/?p=1802')
